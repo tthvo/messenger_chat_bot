04b0cd8cfa8e99b8f07f6b585476a21135103f93
Thuan Tan Vo <carol@Bruh.local>
2020-08-08 17:47:42 -0700

    fixed

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,11 +3,11 @@ import express from "express";
 Config view engine for node app
 */
 
-let configViewEngine = (app) => {
+let viewEngine = (app) => {
     app.use(express.static("./src/public"));
     app.set("view egine", "ejs");
     app.set("views", "./src/views");
 
 };
 
-module.exports = configViewEngine;
+module.exports = viewEngine;
