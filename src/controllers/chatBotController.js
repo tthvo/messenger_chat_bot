50746593743872f2d319fb8492fd7d641a8265cf
Thuan Tan Vo <carol@Bruh.local>
2020-08-09 16:32:18 -0700

    editted

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -137,7 +137,9 @@ function callSendAPI(sender_psid, response) {
     "recipient": {
       "id": sender_psid
     },
-    "message": response
+    "message": {
+      "text": response
+    }
   }
 
   // Send the HTTP request to the Messenger Platform
