c9d8237eeb4df2c4fa894858b80d9c154fafdeac
Thuan Tan Vo <carol@Bruh.local>
2020-08-09 02:33:33 -0700

    editted

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,26 +1,32 @@
 
 
 let postWebhook = (req,res) => {
-    let body = req.body;
+    // Parse the request body from the POST
+  let body = req.body;
 
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
 
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+          /// Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-      });
 
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+      
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 };
 
 let getWebhook = (req,res) => {
@@ -49,6 +55,21 @@ let getWebhook = (req,res) => {
   }
 
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
 
 
 export default {
