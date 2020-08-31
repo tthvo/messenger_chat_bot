76537fc757e2e2eeddfb4caa9ca04dcc76948a3f
Thuan Tan Vo <carol@Bruh.local>
2020-08-31 01:18:38 -0700

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -486,11 +486,11 @@ let listenToStory = (sender_psid, message) => {
                 } else {
                     record -= 1;                 
                 }  
-            } else if (/do you (like|love)/i.test(received_message)) {
+            } else if (/do\s+you\s+(like|love).*/i.test(received_message)) {
                 let response = {"text": "I am not sure ^^"};                
                 await sendMessage(sender_psid, response);
             } 
-            else if (/(feel|am) (better|relieved)/i.test(received_message)) {
+            else if (/(feel|am)\s+(better|relieved).*/i.test(received_message)) {
                 better = true;
                 let response = {"text": "I am happy to hear that! ^^"};                
                 await sendMessage(sender_psid, response);
@@ -501,10 +501,10 @@ let listenToStory = (sender_psid, message) => {
             } else if (/ah+/i.test(received_message)) {
                 let response = {"text": "Did you just moan?"};                
                 await sendMessage(sender_psid, response);
-            } else if (/\s*no+/i.test(received_message)) {
+            } else if (/^\s*no+.*/i.test(received_message)) {
                 let response = {"text": "Oh Okay!😂"};                
                 await sendMessage(sender_psid, response);
-            } else if (/\s*yes+/i.test(received_message)) {
+            } else if (/\s*yes+.*/i.test(received_message)) {
                 let response = {"text": "Hmm..."};                
                 await sendMessage(sender_psid, response);
             } else if(received_message.toLowerCase().includes('done')) {
